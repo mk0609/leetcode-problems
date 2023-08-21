4ee8b7a8451bc509b4ed69226c13f302e45d6e09
MOHIT KUMAR <80811238+mk0609@users.noreply.github.com>
2023-08-21 14:53:13 +0530

    Time: 8 ms (58.70%), Space: 5.9 MB (71.25%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -2,7 +2,16 @@ class Solution {
 public:
     string intToRoman(int num) {
         string roman;
-        vector<string> notations = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+    string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    string ths[]={"","M","MM","MMM","MMMM"};
+        
+    return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        
+        
+        /*vector<string> notations = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         vector<int> value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         
         for(int pos = 0; num>0; ++pos){
@@ -11,7 +20,7 @@ public:
                 num -= value[pos];
             }
         }
-        return roman;
+        return roman;*/
         
  
   
