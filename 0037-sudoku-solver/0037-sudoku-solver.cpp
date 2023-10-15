6084bfc61eac455968900e8f1734e7eb1f25418e
MOHIT KUMAR <80811238+mk0609@users.noreply.github.com>
2023-10-15 23:26:25 +0530

    Update 0037-sudoku-solver.cpp

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
         bool issafe(int val,vector<vector<char>>& board,int row,int col){
-            //row
+          
             int n=board.size();
             for(int i=0;i<n;i++)
                 if((board[row][i]==val)||(board[i][col]==val)||(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)])==val)
@@ -12,14 +12,14 @@ public:
       
         bool solve(vector<vector<char>>& board){
             int n=board.size();
-            //empty cell ko fill
+           
                for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    //check for empty cell
+                    
                     if(board[i][j]=='.')
                     {
                         for(char val='1';val<='9';val++){
-                            //check for safe 
+                            
                             if(issafe(val,board,i,j)){
                                 //insert
                                 board[i][j]=val;
